e846491fa6e51fbab4a7d086c2e70fc31ea37242
MiquelGatius02 <113125375+MiquelGatius02@users.noreply.github.com>
2022-10-16 18:24:45 +0200

    Update

--- filtrar.php
+++ filtrar.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <title>prueba</title>
+    <title>Filtro</title>
 </head>
 
 <body class="bg-secondary">
@@ -29,11 +29,12 @@
     <div class="container bg-white">
         <table class="table table table-striped ">
             <tr>
-                <td scope="col" class="table-dark">id</td>
-                <td scope="col" class="table-dark">nombre</td>
-                <td scope="col" class="table-dark">descripcion</td>
-                <td scope="col" class="table-dark">cantidad</td>
-                <td scope="col" class="table-dark">precio</td>
+                <td scope="col" class="table-dark">Id</td>
+                <td scope="col" class="table-dark">Nombre</td>
+                <td scope="col" class="table-dark">Descripción</td>
+                <td scope="col" class="table-dark">Cantidad</td>
+                <td scope="col" class="table-dark">Precio</td>
+                <td scope="col" class="table-dark">Edit</td>
                 <form action="" method="get">
                     <div class="input-group mb-2">
                         <select name="select" default="Nombre">
@@ -49,12 +50,14 @@
 
             </tr>
             <?php
-            $valor  = $_GET["select"];
-            $filtro = $_GET["filtro"];
+            //datos base de datos
             $servidor = "localhost";
             $usuario = "root";
             $password = "";
             $bd = "actividad1";
+            //Variables
+            $valor  = $_GET["select"];
+            $filtro = $_GET["filtro"];
 
             $con = mysqli_connect($servidor, $usuario, $password, $bd);
 
@@ -62,14 +65,8 @@
                 die("No se ha podido realizar la conexión_" . mysqli_connect_error());
             } else {
                 mysqli_set_charset($con, "utf8");
-                echo $valor;
-                echo  $filtro;
                 $sql2 = "SELECT * FROM `productos` WHERE $valor LIKE '$filtro%' ";
-                echo $valor;
-                echo  $filtro;
                 $consulta = mysqli_query($con, $sql2);
-                echo $valor;
-                echo  $filtro;
                 while ($fila = $consulta->fetch_assoc()) {
                     echo "<tr>";
                     if (str_starts_with($fila[$valor], $filtro)) {
@@ -78,6 +75,9 @@
                         echo "<td>" . $fila["Descripción"] . "</td>";
                         echo "<td>" . $fila["Cantidad"] . "</td>";
                         echo "<td>" . $fila["Precio"] . "</td>";
+                        echo "<td>  <form action='borrar.php?id=". $fila["Id"]."'method='post'>
+                        <input type='submmit' class='btn btn-danger' name = 'borrar' value = 'Borrar'>
+                        </form></td>";
                         echo "</tr>";
                     }
                 }
